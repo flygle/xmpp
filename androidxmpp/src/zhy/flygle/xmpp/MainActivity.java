8860ae89010fda6a64495a5cf829d875aa92115a
flygle <1341582089@qq.com>
2014-10-23 10:17:19 +0800

    Signed-off-by: flygle <1341582089@qq.com>

--- androidxmpp/src/zhy/flygle/xmpp/MainActivity.java
+++ androidxmpp/src/zhy/flygle/xmpp/MainActivity.java
@@ -12,7 +12,6 @@ import org.jivesoftware.smack.packet.Packet;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class MainActivity extends Activity {
 	
